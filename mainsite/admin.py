d89e087292fadd31938f11c463f613ef9cb39b9c
Miqbal20 <miqbal020@gmail.com>
2022-11-12 23:19:16 +0700

    Initial Commit

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from mainsite.models import Data, State
+
 
 # Register your models here.
+class DataAdmin(admin.ModelAdmin):
+    list_display = ['name', 'age']
+    search_fields = ['name', 'age']
+    list_filter = ('id_province',)
+    list_per_page = 10
+
+
+admin.site.register(Data, DataAdmin)
+admin.site.register(State)
+
--- mainsite/models.py
+++ mainsite/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class State(models.Model):
+    province = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.province
+
+
+class Data(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField(null=True)
+    id_province = models.ForeignKey(State, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
